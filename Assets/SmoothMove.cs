e7d8983bfc22999b17e47ad7ee3386ed6d89ec85
Paul Sinnett <paul.sinnett@gmail.com>
2015-02-05 18:00:13 +0000

    smooth move code

--- Assets/SmoothMove.cs
+++ Assets/SmoothMove.cs
@@ -1,62 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SmoothMove : MonoBehaviour
+// This class smooths a change in value over time like SmoothDamp, but it uses an AnimationCurve
+// to hit the target value at the desired time.
+// An instance of SmoothMove can also tell you when it's finished its move.
+public class SmoothMove
 {
-	public AnimationCurve curve;
-	public AnimationCurve positionCurve;
-	public AnimationCurve velocityCurve;
-	float start = 10f;
-	float end = 20f;
-	float velocity = 0f;
+    AnimationCurve curve;
+    float derivative;
+    float current;
+    float time;
 
-	void Start()
-	{
-		Invoke ("Test", 1f);
-	}
+    public SmoothMove()
+    {
+        curve = new AnimationCurve();
+        curve.AddKey(0f, 0f);
+        curve.AddKey(1f, 0f);
+    }
 
-	void Test ()
-	{
-		StartCoroutine (doTest ());
-	}
+    public void Target(float currentValue, float targetValue, float duration)
+    {
+        derivative = 0f;
+        Retarget(currentValue, targetValue, duration);
+    }
 
-	IEnumerator doTest ()
-	{
-		curve = new AnimationCurve ();
-		positionCurve = new AnimationCurve ();
-		velocityCurve = new AnimationCurve ();
-		float value = start;
-		for (float time = 0f; time < 1f; time += Time.deltaTime) 
-		{
-			curve.AddKey(time, value = Smooth (value, ref velocity, end, 0f, 1f - time));
-			yield return null;
-		}
-	}
+    public void Retarget(float currentValue, float targetValue, float duration)
+    {
+        Keyframe[] keys = curve.keys;
+        keys[0].time = 0f;
+        keys[0].value = currentValue;
+        keys[0].inTangent = derivative;
+        keys[0].outTangent = derivative;
+        keys[1].time = duration;
+        keys[1].value = targetValue;
+        keys[1].inTangent = 0f;
+        keys[1].outTangent = 0f;
 
-	float Smooth(float current, ref float currentVelocity, float target, float targetVelocity, float time)
-	{
-		float value = target;
-		if (time > 0f) 
-		{
-			float positionAcceleration = 2f * (target - current - currentVelocity * time) / (time * time);
-			float velocityAcceleration = (targetVelocity - currentVelocity) / time;
-			float acceleration = positionAcceleration;
-			if (Mathf.Abs (velocityAcceleration) > Mathf.Abs (positionAcceleration))
-			{
-				acceleration = velocityAcceleration;
-			}
+        curve.keys = keys;
+        time = 0f;
+        current = currentValue;
+    }
 
-		    value = current + currentVelocity * Time.deltaTime + 0.5f * (acceleration * Time.deltaTime * Time.deltaTime);
-		    currentVelocity = currentVelocity + acceleration * Time.deltaTime;
+    public float Update()
+    {
+        time += Time.deltaTime;
+        float newValue = curve.Evaluate(time);
+        float acceleration = (newValue - current - (derivative * Time.deltaTime)) * (2f / (Time.deltaTime * Time.deltaTime));
+        derivative += acceleration * Time.deltaTime;
+        current = newValue;
+        return current;
+    }
 
-			positionCurve.AddKey(1f - time, positionAcceleration);
-			velocityCurve.AddKey(1f - time, velocityAcceleration);
-		}
-		else
-		{
-			currentVelocity = targetVelocity;
-		}
-
-		return value;
-	}
+    public bool Finished()
+    {
+        return time >= curve.keys[1].time;
+    }
 }
